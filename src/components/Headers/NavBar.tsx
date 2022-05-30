33b0a7cbffa39d9951bb334b5411b268efc6b607
MillionAlgosFather <info@algoworld.io>
2022-05-30 03:42:00 +0200

    feat: tweaking swapper

--- src/components/Headers/NavBar.tsx
+++ src/components/Headers/NavBar.tsx
@@ -225,33 +225,6 @@ const NavBar = () => {
                 ))}
               </Menu>
             </Box>
-            <Icon
-              sx={{ display: { xs: `flex`, md: `none` }, mr: 1 }}
-              fontSize="large"
-            >
-              <Image
-                src="/algoworld_logo.svg"
-                alt="AlgoWorld Swapper Logo"
-                height={40}
-                width={40}
-              />
-            </Icon>
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href=""
-              sx={{
-                mr: 2,
-                display: { xs: `flex`, md: `none` },
-                flexGrow: 1,
-                fontWeight: 700,
-                color: `inherit`,
-                textDecoration: `none`,
-              }}
-            >
-              SWAPPER
-            </Typography>
 
             <Box sx={{ flexGrow: 1, display: { xs: `none`, md: `flex` } }}>
               {pages.map((page) => (
