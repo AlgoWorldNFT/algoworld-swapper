cc60cdbb5ea7d7878b53c4e0a7e170639bbe007f
MillionAlgosFather <info@algoworld.io>
2022-05-30 03:04:54 +0200

    feat: tweaking theme

--- src/redux/theme/darkTheme.ts
+++ src/redux/theme/darkTheme.ts
@@ -3,6 +3,31 @@ import { createTheme } from '@mui/material/styles';
 const darkTheme = createTheme({
   palette: {
     mode: `dark`,
+    primary: {
+      main: `#ff80bf`,
+    },
+    secondary: {
+      main: `#f8f8f2`,
+    },
+    background: {
+      default: `#282a36`,
+      paper: `#44475a`,
+    },
+    text: {
+      primary: `#f8f8f2`,
+    },
+    error: {
+      main: `#FF9580`,
+    },
+    warning: {
+      main: `#FFFF80`,
+    },
+    info: {
+      main: `#8aff80`,
+    },
+    success: {
+      main: `#8aff80`,
+    },
   },
 });
 
