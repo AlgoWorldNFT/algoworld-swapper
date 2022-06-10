d74451d271cb888d101f6f37fc961a6bdd05a414
MillionAlgosFather <info@algoworld.io>
2022-06-10 02:47:19 +0200

    fix: typos

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -21,7 +21,7 @@ export const ALGOEXPLORER_URL: string =
     : `https://testnet.algoexplorer.io`;
 
 export const EMPTY_ASSET_IMAGE_URL = `https://cf-ipfs.com/ipfs/QmXrsy5TddTiwDCXqGc2yzNowKs7WhCJfQ17rvHuArfnQp`;
-export const SWAP_PROXY_VERSION = `0.0.1`;
+export const SWAP_PROXY_VERSION = `0.0.2`;
 
 export const ASA_TO_ASA_FUNDING_FEE = Math.round((0.1 + 0.1 + 0.01) * 1e6);
 export const ASA_TO_ALGO_MAX_FEE = 1_000;
