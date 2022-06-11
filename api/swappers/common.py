0c04a56d9aff5038555ba85e7bbd4b01aef144d0
MillionAlgosFather <info@algoworld.io>
2022-06-11 17:02:03 +0200

    fix: restoring CI; Adding license headers

--- api/swappers/common.py
+++ api/swappers/common.py
@@ -1,3 +1,19 @@
+# AlgoWorld Swapper
+# Copyright (C) 2022 AlgoWorld
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from algosdk.v2client.algod import AlgodClient
 
 INCENTIVE_WALLET = "RJVRGSPGSPOG7W3V7IMZZ2BAYCABW3YC5MWGKEOPAEEI5ZK5J2GSF6Y26A"
