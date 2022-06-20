5f79fd5a3cc9494ac90101251cd5e279f6afe87a
MillionAlgosFather <info@algoworld.io>
2022-06-20 02:27:29 +0200

    chore: testing ci

--- next.config.js
+++ next.config.js
@@ -22,13 +22,9 @@ const moduleExports = {
 };
 
 if (process.env.E2E_TESTS_BASE_URL) {
+  module.exports = moduleExports;
+} else {
   const sentryWebpackPluginOptions = {
-    // Additional config options for the Sentry Webpack plugin. Keep in mind that
-    // the following options are set automatically, and overriding them is not
-    // recommended:
-    //   release, url, org, project, authToken, configFile, stripPrefix,
-    //   urlPrefix, include, ignore
-
     silent: true, // Suppresses all logs
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options.
@@ -37,6 +33,4 @@ if (process.env.E2E_TESTS_BASE_URL) {
   // Make sure adding Sentry options is the last code to run before exporting, to
   // ensure that your source maps include changes from all other Webpack plugins
   module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
-} else {
-  module.exports = moduleExports;
 }
