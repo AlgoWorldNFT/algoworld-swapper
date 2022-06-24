61ff96aae5dfcffde58c6be9e9a4d624d95cbb70
Rene Aavik <renx81@gmail.com>
2022-06-24 15:58:05 +0300

    fix: update lock file

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-const nextJest = require('next/jest')
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
+});
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
@@ -15,7 +15,7 @@ const customJestConfig = {
   },
   testEnvironment: 'jest-environment-jsdom',
   collectCoverageFrom: ['**/*.{ts,tsx}'],
-}
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
