7791810e7f7f6d1c4d26d6586479c424c5489ce3
MillionAlgosFather <info@algoworld.io>
2022-08-01 22:26:58 +0200

    chore: testing ci

--- src/components/Dialogs/ShareSwapDialog.tsx
+++ src/components/Dialogs/ShareSwapDialog.tsx
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { ChainType } from '@/models/Chain';
 import { SwapConfiguration } from '@/models/Swap';
 import { useAppSelector } from '@/redux/store/hooks';
 import getSwapUrl from '@/utils/api/swaps/getSwapUrl';
