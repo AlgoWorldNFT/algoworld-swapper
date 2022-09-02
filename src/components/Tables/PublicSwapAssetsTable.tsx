455923a83ebb9eab8829aab909bd115c87b64436
Altynbek Orumbayev <aorumbayev@protonmail.com>
2022-09-02 16:54:33 +0200

    fix: removing typo

--- src/components/Tables/PublicSwapAssetsTable.tsx
+++ src/components/Tables/PublicSwapAssetsTable.tsx
@@ -32,7 +32,6 @@ const PublicSwapAssetsTable = ({ address, chain }: Props) => {
       }).then(async (response) => {
         const swapConfigurationsForProxy =
           (await response.data) as SwapConfiguration[];
-        console.log(swapConfigurationsForProxy);
         const swapConfigurationAssetsForProxy = swapConfigurationsForProxy.map(
           (config) => {
             return [...config.offering, ...config.requesting];
